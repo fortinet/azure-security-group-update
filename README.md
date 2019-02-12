ae00564d0eda2bceb9397733c50e45e810795e7c
Joel Cripps <jcripps@fortinet.com>
2019-02-12 15:24:06 -0800

    Updated README.md. Changed env variable naming scheme, updated error handling for JSON objects.Updated login to simplify it. Fixed console.log now context.log. License date is fixed
    Change-Id: Ic23ba5f721ccd65bdce1c0c0bdb0a4cfbaa3f02e

--- README.md
+++ README.md
@@ -1,50 +1,53 @@
-# Intro
-This Azure Function works with a FortiGate Automation Action  to change the security group of a requested VM to one specified in an environment variable.
-The function uses Node.js and requires that the resource group be set as an environment variable.
-It works by first fetching information on all the VM's in a resource group, querying their respective network cards and then their attached Public IP Addresses.
+# Introduction
+This Azure function works with a FortiGate Automation Action to change the security group of a requested VM to one specified in an environment variable.
+This function uses **Node.js** and requires that the Resource Group be set as an environment variable.
+It works by first fetching information on all the VM's in a Resource Group, querying their respective network cards and then their attached Public IP Addresses.
 
-    Note: If you have more than one network card attached to a VM this function will not change the security group of all of them, only the network card with the associated IP address.
+> **Note:** If you have more than one network card attached to a VM this function will not change the security group for all of them, only the network card with the associated IP Address.
 
-# Set Up the Azure Function:
-    This function uses Service Principal to authenticate, information on setting up a service principal can be found here: https://docs.microsoft.com/en-us/javascript/azure/node-sdk-azure-authenticate-principal?view=azure-node-latest
-    Additional coding samples can be found here: https://github.com/Azure/azure-sdk-for-node/blob/master/Documentation/Authentication.md
+# Set Up the Azure Function
+This function uses a service principal to authenticate. Information on setting up a service principal can be found in the Microsoft article [Create an Azure service principal with Node.js](https://docs.microsoft.com/en-us/javascript/azure/node-sdk-azure-authenticate-principal?view=azure-node-latest). Additional code samples can be found in the [Azure Github repository](https://github.com/Azure/azure-sdk-for-node/blob/master/Documentation/Authentication.md).
 
-  1. Create a Function APP and select JavaScript as the Runtime Stack
-  2. Create a new Function and Select **HTTP trigger**
-  3. Select the Authorization Level
-  4. Ensure the dependencies are installed:
+  1. In the Azure portal, create a **Function App**. Set the **Runtime Stack** to **JavaScript**.
 
-          ms-rest-azure
-          azure-arm-network
-          azure-arm-compute
-  5. Copy the code into the function or deploy via Visual Studio code
-  6. Under Application Settings in your Function create the following variables and set them with the appropriate value
+     1. Select **Create a resource** from the upper left-hand corner.
+     2. Click **Compute** and then **Function App**.
+     3. Under **Runtime Stack**, select **JavaScript**.
+     4. Select **Windows** as the backend.
+	 4. Click **Create**.
+  2. Create a function in the new function app. Set the **Authorization Level** to **Function**.
+     1. Expand the new function app.
+	 2. Click the **+** button next to **Functions**.
+	 3. If this is the first time you have created a function, click **In-portal** and then **Webhook + API**. To set the **Authorization Level**, expand the newly created function and click **Integrate**. If this is not the first function to be created, click **HTTP trigger**.
+	 4. Under **Authorization Level**, select **Function**.
+  4. Install dependencies by uploading the provided `package.json` and running `npm install` in the **Console** window of the function. Alternatively, install them individually:
+     * `ms-rest-azure`
+     * `azure-arm-network`
+     * `azure-arm-compute`
+  5. Replace the template code with the provided `index.js`. Alternatively, deploy using Visual Studio.
+  6. Select the function app and click **Application settings**.
+  7. Under **Application settings** add the following:
 
-          subscriptionId - Your Subscription ID
-          resourceGroup - The Resource Group in which the VMs are located
-          tenant_id - Domain Or Tenant ID
-          app_id  - Client or App ID
-          app_secret - Password or Secret
-          nsg - the Network Security Group you want to use to quarantine VMs
-
+     * **SUBSCRIPTION_ID:** Your Subscription ID
+     * **RESOURCE_GROUP:** The Resource Group in which the VMs are located
+     * **TENANT_ID:** Domain Or Tenant ID
+     * **REST_APP_ID:** Client or App ID
+     * **REST_APP_SECRET:** Password or Secret
+     * **QUARANTINE_SECURITY_GROUP:** The Network Security Group you want to use to quarantine VMs
 
 # Set Up the FortiGate
 
-  1. Set up your FortiGate by selecting **Security Fabric** and then **Automation.**
-  2. Select **Compromised Host** as the **Trigger**
-  3. Select **Azure Function** as the application
-  4. Set the action parameters, with the **API Gateway** and settings generated in the previous section. Example shown below
+  1. Set up your FortiGate by selecting **Security Fabric** and then **Automation**.
+  2. Select **Compromised Host** as the **Trigger**.
+  3. Select **Azure Function** as the **Action**.
+  4. Set the Action parameters, with the **API Gateway** and settings generated in the previous section. An example is shown below.
 
   ![picture](./imgs/FortigateExampleScreenshot.png)
 
+# Support
+Fortinet-provided scripts in this and other GitHub projects do not fall under the regular Fortinet technical support scope and are not supported by FortiCare Support Services.
+For direct issues, please refer to the [Issues](https://github.com/fortinet/azure-security-group-update/issues) tab of this GitHub project.
+For other questions related to this project, contact [github@fortinet.com](mailto:github@fortinet.com).
 
-
-
-
- # Support
- Fortinet-provided scripts in this and other GitHub projects do not fall under the regular Fortinet technical support scope and are not supported by FortiCare Support Services.
- For direct issues, please refer to the [Issues](https://github.com/fortinet/aws-lambda-vpc-security-group-update/issues) tab of this GitHub project.
- For other questions related to this project, contact [github@fortinet.com](mailto:github@fortinet.com).
-
- ## License
- [License](./LICENSE) © Fortinet Technologies. All rights reserved.
+## License
+[License](./LICENSE) © Fortinet Technologies. All rights reserved.
--- index.js
+++ index.js
@@ -1,136 +1,158 @@
-'use strict'
+'use strict';
 const util = require('util');
 const msRestAzure = require('ms-rest-azure');
 const NetworkManagementClient = require('azure-arm-network');
 const computeManagementClient = require('azure-arm-compute');
+const {
+    SUBSCRIPTION_ID,
+    RESOURCE_GROUP,
+    TENANT_ID,
+    REST_APP_ID,
+    REST_APP_SECRET,
+    QUARANTINE_SECURITY_GROUP
+} = process.env;
 
-
-const subscriptionId = process.env["subscriptionID"];
-const resourceGroup = process.env["resourceGroup"];
-const tenant_id = process.env["tenant_id"];
-const app_id = process.env["app_id"];
-const app_secret = process.env["app_secret"];
-const nsg = process.env["securityGroup"];
-
+const QUARANTINE_SECURITY_GROUP_FQDN = `/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${RESOURCE_GROUP}/providers/Microsoft.Network/networkSecurityGroups/${QUARANTINE_SECURITY_GROUP}`;
 var ipconfigParm;
 var src_ip;
+var isPrivateIP;
 
-
-module.exports = async function (context, req) {
+module.exports = async function(context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-    src_ip = context.bindings.req.body.data.rawlog.srcip;
-    context.log('IP Received:' + src_ip);
+    try {
+        src_ip = req.body.data.rawlog.srcip;
+        isPrivateIP = isPrivateIp(src_ip);
+    } catch (err) {
+        context.log(`Error Retrieving the Source IP${err}`);
+    }
 
-   var quarentine = await getVMs();
+    context.log(`IP Received: ${src_ip}`);
+    const credentials = await msRestAzure.loginWithServicePrincipalSecret(REST_APP_ID, REST_APP_SECRET, TENANT_ID);
+    var nicList = await getNicList();
+    var data = await getMatchingNicAndParams(nicList);
+    if (data) {
+        var matchingNic = data[0];
+        var updateParams = data[1];
+        try {
+            await updateSecurityGroup(RESOURCE_GROUP, matchingNic, updateParams);
+        } catch (err) {
+            context.log(err);
+        }
+    } else {
+        context.log('IP not Found');
+    }
 
+    /**
+     * Update the Security Group.
+     * Will replace current security group.
+     */
+    async function updateSecurityGroup(RESOURCE_GROUP, nic, params) {
+        const client = new NetworkManagementClient(credentials, SUBSCRIPTION_ID);
+        try {
+            var updateNic = await client.networkInterfaces.createOrUpdate(RESOURCE_GROUP, nic, params);
 
-function updateSecurityGroup(resourceGroup, nic,params){
-//Update the Security Group.
-    msRestAzure
-    .loginWithServicePrincipalSecret(app_id,app_secret, tenant_id)
-    .then(credentials => {
-    const client = new NetworkManagementClient(credentials, subscriptionId);
-    return client.networkInterfaces.createOrUpdate(resourceGroup,nic,params);
-     })
-   }
+            if (updateNic.networkSecurityGroup.id === QUARANTINE_SECURITY_GROUP_FQDN) {
+                console.log(`Updated Security Group for ${nic}`);
+            } else {
+                throw new error(`Unable to update security group: ${QUARANTINE_SECURITY_GROUP_FQDN} on nic: ${JSON.stringify(nic)}`);
+            }
+        } catch (err) {
+            console.log(`Error updating the security group: ${err.toString()}`);
+            throw err;
+        }
+    }
 
-async function getNics(nic){
-//different login types here: https://github.com/Azure/azure-sdk-for-node/tree/master/runtime/ms-rest-azure
-//exceptions need to be done on this
-    msRestAzure
-    .loginWithServicePrincipalSecret(app_id, app_secret, tenant_id)
-    .then(credentials => {
-    const client = new NetworkManagementClient(credentials, subscriptionId);
-    client.networkInterfaces.get(resourceGroup,nic,(async function (err, result) {
-        if (err) {
-          console.log(util.format('\n Error  ' +
-            'the current subscription:\n%s', util.inspect(err, { depth: null })));
+    async function getMatchingNicAndParams(niclist) {
+        const client = new NetworkManagementClient(credentials, SUBSCRIPTION_ID);
+        try {
+            for (let nic of niclist) {
+                var result = await client.networkInterfaces.get(RESOURCE_GROUP, nic);
+                if (result && result.ipConfigurations) {
+                    for (let networkData of result.ipConfigurations) {
+                        ipconfigParm = result.ipConfigurations;
+                        var regionName = result.location;
+                        // Parameters for security group update
+                        const params = {
+                            location: regionName,
+                            networkSecurityGroup: {
+                                id: QUARANTINE_SECURITY_GROUP_FQDN
+                            },
+                            ipConfigurations: ipconfigParm
+                        };
+                        if (isPrivateIP === true) {
+                            if (networkData.privateIPAddress === src_ip) {
+                                return [nic, params];
+                            }
+                        } else {
+                            if (networkData.publicIPAddress) {
+                                var [ipName] = networkData.publicIPAddress.id.split('/').slice(-1);
+                                var publicIP = await getPublicIp(ipName);
+                                if (publicIP === src_ip) {
+                                    context.log(`${publicIP} Found`);
+                                    return [nic, params];
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (error) {
+            context.log(util.format('\n Error  the current subscription:\n%s', util.inspect(err, {
+                depth: null
+            })));
+        }
+    }
+
+    async function getPublicIp(ipName) {
+        const networkClient = new NetworkManagementClient(credentials, SUBSCRIPTION_ID);
+        try {
+            var result = await networkClient.publicIPAddresses.get(RESOURCE_GROUP, ipName);
+            return result && result.ipAddress;
+        } catch (error) {
+            context.log(util.format('\n Error listing Public IPs ' +
+                'the current subscription:\n%s', util.inspect(err, {
+                depth: null
+            })));
+        }
+    }
+
+    async function getNicList() {
+        const networkClient = new computeManagementClient(credentials, SUBSCRIPTION_ID);
+        var result = await networkClient.virtualMachines.list(RESOURCE_GROUP);
+        try {
+            var list1 = [];
+            var list2 = [];
+            for (var i = 0; i < result.length; i++) {
+                var object = result[i];
+                for (let NetworkData of object.networkProfile.networkInterfaces) {
+                    list1.push(NetworkData.id);
+                }
+            }
+            for (var j in list1) {
+                var indexItem = list1[j];
+                var lastindex = indexItem.lastIndexOf('/');
+                var result = indexItem.substring(lastindex + 1);
+                list2.push(result);
+            }
+            return (list2);
+        } catch (err) {
+            context.log(util.format('\n Error  ' +
+                'the current subscription:\n%s', util.inspect(err, {
+                depth: null
+            })));
+        }
+
+    }
+
+    function isPrivateIp(IPaddress) {
+        var SplitIP = IPaddress.split('.');
+        if (SplitIP[0] === '10' ||
+            (SplitIP[0] === '172' && (parseInt(SplitIP[1], 10) >= 16 && parseInt(SplitIP[1], 10) <= 31)) ||
+            (SplitIP[0] === '192' && SplitIP[1] === '168')) {
+            return true;
         } else {
-           try{
-            var ipLocation = result.ipConfigurations[0].publicIPAddress.id
-            var indexItem = ipLocation;
-            var lastindex = indexItem.lastIndexOf('/');
-            var ipName = indexItem.substring(lastindex +1);
-
-            ///Parameters for security group update
-            ipconfigParm = result.ipConfigurations[0]
-            var regionName = result.location;
-            const params = {
-              location: regionName,
-              networkSecurityGroup :{
-
-                  id:nsg
-
-
-              },
-               ipConfigurations: [ipconfigParm]
-                       };
-
-            listAllPublicIPs(ipName,nic,params);
-            return nic;
-           }
-           catch(err){
-            console.log("Error, probably doesn't exsist. Error message: "+ err)
-            }
+            return false;
         }
-    }));
-
-})};
-
-    function listAllPublicIPs (ipName,nic,params) {
-        msRestAzure
-        .loginWithServicePrincipalSecret(app_id, app_secret, tenant_id)
-        .then(credentials => {
-         const networkClient = new NetworkManagementClient(credentials,subscriptionId);
-        console.log('\n Find Public IPs');
-        networkClient.publicIPAddresses.get(resourceGroup,ipName,((function (err, result) {
-          if (err) {
-            console.log(util.format('\n Error listing Public IPs ' +
-              'the current subscription:\n%s', util.inspect(err, { depth: null })));
-
-          } else {
-            if (result.ipAddress == src_ip ){
-              updateSecurityGroup(resourceGroup,nic,params);
-              return nic
-            }
-            else{
-              console.log("IpAddress: " + src_ip +" Not found")
-              return -1;
-            }
-        }
-
-        })));
-        })
-    };
-   async function getVMs (callback) {
-    msRestAzure
-    .loginWithServicePrincipalSecret(app_id, app_secret, tenant_id,callback)
-    .then(credentials => {
-     const networkClient = new computeManagementClient(credentials,subscriptionId);
-    console.log('\n List IPs under the resource group.');
-     networkClient.virtualMachines.list(resourceGroup,(async function (err, result) {
-      if (err) {
-        console.log(util.format('\n Error  ' +
-          'the current subscription:\n%s', util.inspect(err, { depth: null })));
-      } else {
-          var list1 = [];
-          var list2 = [];
-          for (var i = 0; i <result.length; i++){
-            var object = result[i];
-            list1.push(object.networkProfile.networkInterfaces[0].id);
-          }
-          for(var j in list1){
-              var indexItem = list1[j];
-              var lastindex = indexItem.lastIndexOf('/');
-              var result = indexItem.substring(lastindex +1);
-              list2.push(result);
-        }
-            for (var k in list2){
-              getNics(list2[k]);
-             }
-        return list2;
-        }
-    }));
-});
-   };
+    }
 };
